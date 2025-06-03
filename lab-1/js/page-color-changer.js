41f46b4ff1832573b6c481844dda0d52d6887ad6
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-06-03 08:51:33 -0400

    x

--- lab-1/js/page-color-changer.js
+++ lab-1/js/page-color-changer.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-var redVal = document.querySelector('input#red');
-var greenVal = document.querySelector('input#green');
-var blueVal = document.querySelector('input#blue');
+document.addEventListener("DOMContentLoaded", function () {
+    var redVal = document.querySelector('input#red');
+    var greenVal = document.querySelector('input#green');
+    var blueVal = document.querySelector('input#blue');
 
-function updateBackground()
-{
-    var r = redVal.value;
-    var g = greenVal.value;
-    var b = blueVal.value;
+    function updateBackground() {
+        var r = redVal.value;
+        var g = greenVal.value;
+        var b = blueVal.value;
 
-    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+        document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 
-}
+    }
 
 
-redVal.addEventListener('input', updateBackground);
-greenVal.addEventListener('input', updateBackground);
-blueVal.addEventListener('input', updateBackground);
+    redVal.addEventListener('input', updateBackground);
+    greenVal.addEventListener('input', updateBackground);
+    blueVal.addEventListener('input', updateBackground);
 });
